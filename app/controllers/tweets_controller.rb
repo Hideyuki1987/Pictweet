e8a8909d3f0fbe60b4fe553fa04142d6bb1847fe
Hideyuki ."F".Asaka <carm415@hotmail.com>
2017-06-26 19:15:18 +0900

    Set private mode

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,19 @@
 class TweetsController < ApplicationController
 
-	def index
-	  @tweets = Tweet.all
-	end
+  def index
+      @tweets = Tweet.all
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-      Tweet.create(name: params[:name], image: params[:image], text: params[:text])
-    end
-	
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
